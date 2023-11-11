3238736dae548e1e6bd3a3267bcf7ee1c0168384
Edgar Cárdenas <edcardenashorta@gmail.com>
2023-11-11 19:28:05 -0300

    setting up the Laudus services

---
+++ app/services/laudus_api_service.rb
@@ -0,0 +1,48 @@
+require 'rest-client'
+require 'json'
+
+class LaudusApiService
+  BASE_URL = "https://api.laudus.cl"
+
+  def initialize(username, password, company_vat_id)
+    @username = username
+    @password = password
+    @company_vat_id = company_vat_id
+    @token = get_token
+  end
+
+  def get_token
+    response = RestClient.post "#{BASE_URL}/security/login", {
+      userName: @username,
+      password: @password,
+      companyVATId: @company_vat_id
+    }.to_json, { content_type: :json, accept: :json }
+
+    JSON.parse(response.body)["token"]
+  rescue RestClient::ExceptionWithResponse => e
+    e.response
+  end
+
+  def get_clients(filters = {})
+    body = {
+      options: {
+        offset: 0,
+        limit: 50
+      },
+      fields: ["customerId", "legalName", "VATId"],
+      filterBy: filters,
+      orderBy: [{ field: "VATId", direction: "ASC" }]
+    }.to_json
+
+    response = RestClient.post "#{BASE_URL}/sales/customers/list", body, headers
+    JSON.parse(response.body)
+  rescue RestClient::ExceptionWithResponse => e
+    e.response
+  end
+
+  private
+
+  def headers
+    { 'Authorization' => "Bearer #{@token}", 'Content-Type' => 'application/json' }
+  end
+end
