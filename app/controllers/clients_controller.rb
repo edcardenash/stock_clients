2aca142141f6a685d6ba4a7320c437bbf8ae61b2
Edgar Cárdenas <edcardenashorta@gmail.com>
2023-11-12 17:44:26 -0300

    updating

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -22,17 +22,22 @@ class ClientsController < ApplicationController
     customer_id = params[:id].to_i
     invoices_list = api_service.get_invoices_list_by_customer(customer_id)
 
-    @purchase_products = []
+    products_hash = {}
     invoices_list.each do |invoice|
       invoice_id = invoice['salesInvoiceId']
       invoice_details = api_service.get_invoice_details(invoice_id)
+      next unless invoice_details && invoice_details['items']
+
       invoice_details['items'].each do |item|
         product_id = item['product']['productId']
+        next if products_hash[product_id]
+
         product_details = api_service.get_product(product_id)
         stock_details = api_service.get_stock_product(product_id)
-        @purchase_products << { product: product_details, stock: stock_details['stock'] }
+        products_hash[product_id] = { product: product_details, stock: stock_details['stock'] }
       end
     end
+    @purchase_products = products_hash.values
   end
 
 
