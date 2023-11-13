29d3fcabd74750f38eeaf9b17fb3275ed05bce17
Edgar Cárdenas <edcardenashorta@gmail.com>
2023-11-13 17:42:39 -0300

    setting job to update invoices and products

---
+++ app/models/invoice.rb
@@ -0,0 +1,5 @@
+class Invoice < ApplicationRecord
+  belongs_to :client
+  has_many :invoice_products
+  has_many :products, through: :invoice_products
+end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
   has_many :client_products
   has_many :clients, through: :client_products
+  has_many :invoice_products
+  has_many :invoices, through: :invoice_products
 end
